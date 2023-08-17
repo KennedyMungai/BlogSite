6a01bd233824497955d63dc203ad3ded66731993
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-08-17 09:43:42 +0300

    Updated the markup for the first page of the application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
 		<main className='px-6 mx-auto'>
 			<p className='mt-12 mb-12 text-3xl text-center dark:text-white'>
-				Hello and Welcome👋{' '}
+				Hello and Welcome👋
 				<span className='whitespace-nowrap'>
 					I am <span className='font-bold'>John</span>
 				</span>
