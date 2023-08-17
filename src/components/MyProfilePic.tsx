3c9294b3546fb11a50468448a073f5d80f555fb9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-08-17 10:04:53 +0300

    Created the component file for the profile pic

---
+++ src/components/MyProfilePic.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+type Props = {}
+
+const MyProfilePic = (props: Props) => {
+	return (
+		<section className='w-full mx-auto'>
+			<Image
+				className='border-4 border-black dark:border-slate-700 drop-shadow-xl shadow-black rounded-full mx-auto mt-8'
+				src={'/images/HoneyBadger.jpg'}
+				width={'200'}
+				height={'200'}
+				alt='Profile Pic'
+				priority={true}
+				loading='lazy'
+			></Image>
+		</section>
+	)
+}
+
+export default MyProfilePic
