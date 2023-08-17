eda4372a125c40ba27d3b68374e8aee24f8b95c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-08-17 11:43:34 +0300

    Formatted the posts items

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,4 +1,5 @@
 import { getSortedPostsData } from '@/lib/posts'
+import ListItem from './ListItem'
 
 type Props = {}
 
@@ -9,7 +10,9 @@ const Posts = (props: Props) => {
 		<section className='mt-6 mx-auto max-w-2xl'>
 			<h2 className='text-4xl font-bold dark:text-white/90'>Blog</h2>
 			<ul className='w-full'>
-				{posts.map((post) => JSON.stringify(post))}
+				{posts.map((post) => (
+					<ListItem key={post.id} post={post} />
+				))}
 			</ul>
 		</section>
 	)
