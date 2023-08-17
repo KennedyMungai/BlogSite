532b2d1f4fa189fc86869760e90d324cf9af7c1a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-08-17 10:05:50 +0300

    Rendered the profile pic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
+import MyProfilePic from '@/components/MyProfilePic'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={montserrat.className}>
 				<Navbar />
+				<MyProfilePic />
 				{children}
 			</body>
 		</html>
