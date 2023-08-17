56b54898c9f1cc66d8c58e0a396a2e1998d06038
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-08-17 10:08:54 +0300

    Put the Link inside a h1 tag

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,14 @@ const Navbar = () => {
 	return (
 		<nav className='bg-slate-800 p-4 sticky top-0 drop-shadow-xl z-10'>
 			<div className='prose prose-xl mx-auto flex justify-between flex-col sm:flex-row'>
-				<Link
-					href={'/'}
-					className='text-white/90 no-underline hover:text-white'
-				>
-					John Mungai
-				</Link>
+				<h1 className='text-3xl font-bold text-white flex justify-center'>
+					<Link
+						href={'/'}
+						className='text-white/90 no-underline hover:text-white'
+					>
+						John Mungai
+					</Link>
+				</h1>
 			</div>
 		</nav>
 	)
